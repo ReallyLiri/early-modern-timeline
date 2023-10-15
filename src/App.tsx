9459c2ebbcafb732da6720c7feb0c663f0040146
ReallyLiri <liri@diversion.dev>
2023-10-15 20:14:14 +0300

    timeline wip

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,58 @@
-import { Timeline } from "./components/Timeline";
-import { useEffect, useState } from "react";
+import { Timeline, TimelineNode } from "./components/Timeline";
+import { useEffect, useMemo, useState } from "react";
 import { TimelineEvent } from "./data/TimelineEvent";
 import { fetchDataAsync } from "./data/data";
+import { sortedUniq } from "lodash";
+
+const nodeTitle = (events: TimelineEvent[]) => {
+  if (events.length === 1) {
+    return events[0].title;
+  }
+  return `${events[0].title} +${events.length - 1} more...`;
+};
 
 function App() {
   const [events, setEvents] = useState<TimelineEvent[]>([]);
+  const [filteredEvents, setFilteredEvents] = useState<TimelineEvent[]>([]);
 
   useEffect(() => {
     fetchDataAsync().then((data) => setEvents(data.events));
   }, []);
 
+  useEffect(() => {
+    setFilteredEvents(events.filter((event) => !!event.year));
+  }, [events]);
+
+  const eventsByYear: Record<number, TimelineEvent[]> = useMemo(
+    () =>
+      filteredEvents.reduce(
+        (acc, event) => {
+          acc[event.year] = [...(acc[event.year] || []), event];
+          return acc;
+        },
+        {} as Record<number, TimelineEvent[]>,
+      ),
+    [filteredEvents],
+  );
+
+  const years = useMemo(
+    () => sortedUniq(filteredEvents.map((event) => event.year).sort()),
+    [filteredEvents],
+  );
+
+  const nodes: TimelineNode[] = useMemo(
+    () =>
+      years.map((year) => ({
+        year,
+        tags: sortedUniq(eventsByYear[year].flatMap((event) => event.tags).sort()),
+        title: nodeTitle(eventsByYear[year]),
+      })),
+    [eventsByYear, years],
+  );
+
   return (
     <>
-      <Timeline events={events} />
+      <Timeline nodes={nodes} onYearSelected={() => {}} />
     </>
   );
 }
--- src/components/Timeline.tsx
+++ src/components/Timeline.tsx
@@ -1,43 +1,76 @@
-import { TimelineEvent } from "../data/TimelineEvent";
-import { useElementSize, useWindowSize } from "usehooks-ts";
-import React, { useEffect, useMemo, useRef, useState } from "react";
-import { sortedUniq } from "lodash";
-import styled from "styled-components";
+import React from "react";
+import styled, { css } from "styled-components";
 import { Transformable } from "./Transformable";
 
-type Props = {
-  events: TimelineEvent[];
+export type TimelineNode = {
+    year: number;
+    tags: string[];
+    title: string;
 };
 
+type Props = {
+    nodes: TimelineNode[];
+    onYearSelected: (year: number) => void;
+};
+
+const NodeWidthRem = 2
+const NodeYearDiffPx = 10
+
 const Container = styled(Transformable)`
-  height: 320px;
   width: fit-content;
-  background: green;
+  border: solid black;
+  border-width: 1px 0;
+  margin: 2rem 0;
+  padding: 2rem 0;
 `;
 
 const Row = styled.div`
   display: flex;
   flex-direction: row;
   gap: 1rem;
+  padding: 0 1rem;
 `;
 
-export const Timeline = ({ events }: Props) => {
-  const years = useMemo(
-    () => sortedUniq(events.map((event) => event.year).sort()),
-    [events],
-  );
+const HorizontalLine = styled.div`
+  position: relative;
+  height: 4px;
+  background-color: red;
+  top: ${ NodeWidthRem / 2 }rem;
+  z-index: 0;
+`;
 
-  const yearsWat = years
-    .flatMap((year) => [year - 1, year, year + 1])
-    .filter((y) => !isNaN(y));
-  return (
-    <Container>
-      <Row>
-        {" "}
-        {yearsWat.map((year) => (
-          <div key={year}>{year}</div>
-        ))}
-      </Row>
-    </Container>
-  );
+const YearNode = styled.div<{ diffFromPrevYear: number }>`
+  cursor: pointer;
+  height: ${ NodeWidthRem }rem;
+  width: ${ NodeWidthRem }rem;
+  border-radius: 50%;
+  background-color: green;
+  z-index: 1;
+  margin-left: ${ ({ diffFromPrevYear }) => ( diffFromPrevYear - 1 ) * NodeYearDiffPx }px;
+`;
+
+const YearNodeTitle = styled.div<{index: number}>`
+  position: relative;
+  width: 0;
+  font-size: 0.8rem;
+  text-wrap: nowrap;
+  ${({index}) => index %2 === 0 && css`top: 2rem;`}
+  ${({index}) => index %2 === 1 && css`bottom: 1rem;`}
+`
+
+export const Timeline = ({ nodes }: Props) => {
+    return (
+        <Container>
+        <HorizontalLine/>
+        <Row>
+            { " " }
+            { nodes.map((node, index) => (
+                <>
+                    <YearNodeTitle index={index}>{ node.title }</YearNodeTitle>
+                    <YearNode key={ node.year } title={ `${ node.year }` } diffFromPrevYear={ index === 0 ? 1 : node.year - nodes[index - 1].year }/>
+                </>
+            )) }
+        </Row>
+        </Container>
+    );
 };
