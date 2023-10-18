ea4f1401bb86905b4197501087921a54d8d59b8b
MiaMish <anon@anon.com>
2023-10-18 08:12:42 +0300

    seperate jsons, enhance cli

--- scripts/validate_json.py
+++ scripts/validate_json.py
@@ -1,25 +1,28 @@
+import functools
 import json
 from jsonschema import validate
 
-json_file = '/Users/mia/dev/personal/early-modern-timeline/public/events.json'
-json_schema_file = '/Users/mia/dev/personal/early-modern-timeline/scripts/events.schema.json'
+json_file = 'public/data/{}.json'
+json_schema_file = 'scripts/schemas/{}.schema.json'
 
 
-def validate_events():
-    events_schema = json.load(open(json_schema_file))
-    events = json.load(open(json_file))
-    validate(events, events_schema)
+def validate_events(validate_type: str):
+    schema = json.load(open(json_schema_file.format(validate_type)))
+    data = json.load(open(json_file.format(validate_type)))
+    validate(data, schema)
 
 
-def validation_wrapper(func):
-    print("Starting validation")
+def validation_wrapper(func, description: str = None):
+    print(f"Starting validation: {description}")
     try:
         func()
-        print("Validation successful")
+        print(f"Validation successful: {description}")
     except Exception as e:
         print("Validation failed with error: " + str(e))
         exit(1)
 
 
 if __name__ == '__main__':
-    validation_wrapper(validate_events)
+    validation_wrapper(functools.partial(validate_events, 'events'), 'events')
+    validation_wrapper(functools.partial(validate_events, 'tags'), 'tags')
+    validation_wrapper(functools.partial(validate_events, 'communities'), 'communities')
