7b76ec8540b54cf3afbd9e59515893195b603ae1
MiaMish <anon@anon.com>
2023-10-18 07:03:59 +0300

    fix a few events, add a validation script

---
+++ scripts/validate_json.py
@@ -0,0 +1,15 @@
+import json
+from jsonschema import validate
+
+json_file = '/Users/mia/dev/personal/early-modern-timeline/public/events.json'
+json_schema_file = '/Users/mia/dev/personal/early-modern-timeline/scripts/events.schema.json'
+
+
+def validate_events():
+    events_schema = json.load(open(json_schema_file))
+    events = json.load(open(json_file))
+    validate(events, events_schema)
+
+
+if __name__ == '__main__':
+    validate_events()
