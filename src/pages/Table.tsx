8b7a3301e358546c7a92d75aad1b372b55e6b4ac
ReallyLiri <liri@diversion.dev>
2023-10-19 17:20:00 +0300

    Filters

--- src/pages/Table.tsx
+++ src/pages/Table.tsx
@@ -1,27 +1,24 @@
 import { TimelineEvent } from "../data/data";
 import {
-  Column,
   ColumnSizingState,
   createColumnHelper,
-  FilterFn,
   flexRender,
   getCoreRowModel,
   getFilteredRowModel,
   getSortedRowModel,
-  Row,
   SortingState,
   Table,
   useReactTable,
+  VisibilityState,
 } from "@tanstack/react-table";
-import { isArray, startCase } from "lodash";
-import styled from "styled-components";
-import { ReactNode, useCallback, useEffect, useRef, useState } from "react";
+import { isArray, isEmpty, startCase } from "lodash";
+import styled, { css } from "styled-components";
+import { ReactNode, useEffect, useMemo, useRef, useState } from "react";
 import { BACKGROUND_COLOR, MAIN_COLOR, SECONDARY_COLOR } from "../theme";
 import { Anchor } from "../components/Anchor";
 import { Tag } from "../components/Tag";
 import { FlexFiller } from "../components/FlexFiller";
 import { DebouncedInput } from "../components/DebouncedInput";
-import { FilterFnOption } from "@tanstack/table-core/src/features/Filters";
 
 type EventField = keyof TimelineEvent;
 type EventFieldValue = TimelineEvent[keyof TimelineEvent];
@@ -79,6 +76,7 @@ const Box = styled.div<{ width?: number }>`
   gap: 1rem;
   border-radius: 1rem;
   background-color: ${BACKGROUND_COLOR};
+  align-items: center;
 `;
 
 const StyledTable = styled.table`
@@ -185,7 +183,80 @@ const renderCell = (field: EventField, value: EventFieldValue): ReactNode => {
   return startCase(value.toString());
 };
 
+const VisibleOnlyCheckbox = styled.div`
+  display: flex;
+  flex-direction: row;
+  font-size: 0.8rem;
+  text-wrap: nowrap;
+  align-items: center;
+  gap: 0.25rem;
+`;
+
+const Label = styled.span`
+  font-size: 0.8rem;
+`
+
+const Controls = styled.div`
+display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-items: center;
+  width: 100%;
+`
+
+const ControlsRows = styled.div<{collapsed: boolean}>`
+  ${({collapsed}) => collapsed && css`
+    height: 0;
+    opacity: 0;
+  `};
+  display: flex;
+  flex-direction: column;
+  gap: 0.5rem;
+`
+
+const CollapseButton = styled.div<{collapse: boolean}>`
+  align-self: center;
+  justify-self: center;
+  text-align: center;
+  cursor: pointer;
+  width: fit-content;
+  background-color: ${BACKGROUND_COLOR};
+  color: ${MAIN_COLOR};
+  border-radius: 1rem;
+  padding: 1rem;
+  font-size: 2rem;
+  border: 2px solid ${SECONDARY_COLOR};
+  margin-left: 2rem;
+  transform: rotate(${({collapse}) => collapse ? 90 : -90}deg);
+`
+
+const filterEvents = (
+  events: TimelineEvent[],
+  visibility: VisibilityState,
+  freeText: string,
+  freeTextOnlyForVisible: boolean,
+  yearsRange: [number, number],
+) => {
+  return events.filter((ev) => {
+    if (!isEmpty(freeText)) {
+      const props: TimelineEvent = { ...ev };
+      if (freeTextOnlyForVisible) {
+        Object.keys(visibility)
+          .filter((key) => !visibility[key])
+          .forEach((key) => delete props[key as EventField]);
+      }
+      const json = JSON.stringify(props).toLocaleLowerCase();
+      if (!json.includes(freeText.toLocaleLowerCase())) {
+        return false;
+      }
+    }
+
+    return !( ev.year < yearsRange[0] || ev.year > yearsRange[1] );
+  });
+};
+
 export const TimelineTable = ({ events }: Props) => {
+  const [filteredEvents, setFilteredEvents] = useState<TimelineEvent[]>([]);
   const [columnVisibility, setColumnVisibility] = useState(
     defaultVisibleColumns as Partial<Record<EventField, boolean>>,
   );
@@ -196,13 +267,45 @@ export const TimelineTable = ({ events }: Props) => {
     })),
   );
   const [columnSizing, setColumnSizing] = useState<ColumnSizingState>({});
+  const [globalFilterVisibleOnly, setGlobalFilterVisibleOnly] = useState(false);
   const [globalFilter, setGlobalFilter] = useState("");
+  const minYear = useMemo(
+    () => Math.min(...events.map((ev) => ev.year)),
+    [events],
+  );
+  const maxYear = useMemo(
+    () => Math.max(...events.map((ev) => ev.year)),
+    [events],
+  );
+  const [yearsRange, setYearsRange] = useState<[number, number]>([0,0]);
   const visibilityBoxRef = useRef<HTMLDivElement>(null);
+  const [controlsCollapsed, setControlsCollapsed] = useState<boolean>(false)
+
+  useEffect(() => {
+    setYearsRange([minYear, maxYear])
+  }, [maxYear, minYear]);
+
+  useEffect(() => {
+    setFilteredEvents(
+      filterEvents(
+        events,
+        columnVisibility,
+        globalFilter,
+        globalFilterVisibleOnly,
+        yearsRange,
+      ),
+    );
+  }, [
+    columnVisibility,
+    events,
+    globalFilter,
+    globalFilterVisibleOnly,
+    yearsRange,
+  ]);
 
   const table: Table<TimelineEvent> = useReactTable({
-    data: events,
+    data: filteredEvents,
     columns,
-    getColumnCanGlobalFilter: () => true,
     getCoreRowModel: getCoreRowModel(),
     getSortedRowModel: getSortedRowModel(),
     getFilteredRowModel: getFilteredRowModel(),
@@ -210,7 +313,6 @@ export const TimelineTable = ({ events }: Props) => {
       columnVisibility,
       sorting: columnSorting,
       columnSizing,
-      globalFilter,
     },
     onColumnVisibilityChange: setColumnVisibility,
     onSortingChange: setColumnSorting,
@@ -228,29 +330,60 @@ export const TimelineTable = ({ events }: Props) => {
 
   return (
     <Container>
-      <Box ref={visibilityBoxRef}>
-        {table.getAllLeafColumns().map((column) => {
-          return (
-            <div key={column.id}>
+      <Controls>
+        <ControlsRows collapsed={controlsCollapsed}>
+          <Box ref={visibilityBoxRef}>
+            {table.getAllLeafColumns().map((column) => {
+              return (
+                  <div key={column.id}>
+                    <input
+                        type="checkbox"
+                        checked={column.getIsVisible()}
+                        onChange={column.getToggleVisibilityHandler()}
+                    />{" "}
+                    {startCase(column.id)}
+                  </div>
+              );
+            })}
+          </Box>
+          <Box width={visibilityBoxRef.current?.clientWidth}>
+            <StyledInput
+                value={globalFilter ?? ""}
+                onChange={(value) => setGlobalFilter(String(value))}
+                placeholder={`Search ${
+                    globalFilterVisibleOnly ? "visible" : "all"
+                } columns...`}
+            />
+            <VisibleOnlyCheckbox>
+              <span>Visible only:</span>
               <input
-                {...{
-                  type: "checkbox",
-                  checked: column.getIsVisible(),
-                  onChange: column.getToggleVisibilityHandler(),
-                }}
-              />{" "}
-              {startCase(column.id)}
-            </div>
-          );
-        })}
-      </Box>
-      <Box width={visibilityBoxRef.current?.clientWidth}>
-        <StyledInput
-          value={globalFilter ?? ""}
-          onChange={(value) => setGlobalFilter(String(value))}
-          placeholder="Search all columns..."
-        />
-      </Box>
+                  type="checkbox"
+                  checked={globalFilterVisibleOnly}
+                  onChange={() => setGlobalFilterVisibleOnly((check) => !check)}
+              />
+            </VisibleOnlyCheckbox>
+          </Box>
+          <Box width={visibilityBoxRef.current?.clientWidth}>
+            <Label>From:</Label>
+            <StyledInput
+                onChange={year => setYearsRange(r => [year as number, r[1]])}
+                value={yearsRange[0]}
+                type="number"
+                max={maxYear}
+                min={minYear}
+            />
+            <Label>Until:</Label>
+            <StyledInput
+                onChange={year => setYearsRange(r => [r[0], year as number])}
+                value={yearsRange[1]}
+                type="number"
+                max={maxYear}
+                min={minYear}
+            />
+          </Box>
+        </ControlsRows>
+          <CollapseButton title={`${controlsCollapsed ? "Expand" : "Collapse"} controls`} collapse={controlsCollapsed} onClick={() => setControlsCollapsed(b => !b)}>➤</CollapseButton>
+      </Controls>
       <StyledTable>
         <thead>
           {table.getHeaderGroups().map((headerGroup) => (
